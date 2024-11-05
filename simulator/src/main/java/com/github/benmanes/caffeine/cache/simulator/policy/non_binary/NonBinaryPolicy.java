bb3aebdce0881eaa6f63bb4d716018ebd6ace5e2
Shahar Blank <shaharblank1@gmail.com>
2024-11-05 23:46:07 +0200

    Add full algorithm and record relevant statistics

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/non_binary/NonBinaryPolicy.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/non_binary/NonBinaryPolicy.java
@@ -9,8 +9,9 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 
 import javax.annotation.Nullable;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Stack;
 
@@ -18,8 +19,9 @@ import java.util.Stack;
 public final class NonBinaryPolicy implements Policy {
   final Long2ObjectMap<Prefix> data;
   final long maximumSize; // in chunks
-  long currentSize; // in chunks
-  long currentRequest;
+  long currentSize;
+
+  final PolicyStats policyStats;
 
   final long BANDWIDTH = 1250; // in MBps
   static final double MEAN = 0.2; // average delay in seconds (e.g., 200 ms)
@@ -29,29 +31,39 @@ public final class NonBinaryPolicy implements Policy {
 
   public NonBinaryPolicy(Config config) {
     var settings = new BasicSettings(config);
+    this.policyStats = new PolicyStats("Non-Binary");
+
     this.data = new Long2ObjectOpenHashMap<>();
     this.maximumSize = settings.maximumSize();
     this.currentSize = 0;
-    this.currentRequest = 0;
-    this.random = new Random();
+
+    this.random = new Random(1337);
   }
 
   @Override
   public void record(AccessEvent event) {
-    currentRequest++;
-
     long itemKey = event.key();
-    Prefix old = data.get(itemKey);
-    if (old != null) {
-      // TODO: stat and document partial hit
-    } else {
-      // TODO: stat and document full miss
-    }
-    Prefix currentPrefix = old == null ? new Prefix(itemKey, currentRequest) : old;
-    long idealSize = (long) (BANDWIDTH * sampleSourceProcessingTime()) + 1;
+    Optional<Prefix> existing = Optional.ofNullable(data.getOrDefault(itemKey, null));
+    policyStats.recordOperation();
+
+    Prefix currentPrefix = existing.orElseGet(() -> new Prefix(itemKey));
+    double sourceDelay = sampleSourceProcessingTime();
+    // The ideal prefix size - the number of chunks that give the "no delay" illusion
+    long idealSize = (long) (calculateDelay(sourceDelay, currentPrefix.size(), BANDWIDTH) * BANDWIDTH);
+    recordPartialHitStats(currentPrefix, idealSize);
+
     insertChunks(currentPrefix, idealSize);
     if (currentPrefix.size() > 0) {
       data.put(itemKey, currentPrefix);
+      policyStats.recordOperation();
+    }
+  }
+
+  private void recordPartialHitStats(Prefix old, long idealSize) {
+    policyStats.addHits(old.size());
+    if (old.size() < idealSize) {
+      // underflow case
+      policyStats.addMisses(idealSize - old.size());
     }
   }
 
@@ -60,48 +72,121 @@ public final class NonBinaryPolicy implements Policy {
   }
 
   private void insertChunks(Prefix prefix, long idealSize) {
-    // try to insert more chunks until we reach ideal prefix size,
+    // try to insert more chunks until we reach ideal fatherPrefix size,
     //  or we stop due to not benefiting from it
 
     while (true) {
-      if (prefix.size() == idealSize) {
-        break;
-      }
-
-      ArrayList<Chunk> endChunks = getAllEndChunks();
-      Chunk newChunk = new Chunk(prefix.itemKey, currentRequest);
-      Chunk victim = findVictim(newChunk, endChunks);
-      if (victim == null) {
-        break;
-      }
-
-      Prefix victimPrefix = data.get(victim.itemKey);
-      victimPrefix.chunks.pop();
-      if (victimPrefix.size() == 0) {
-        data.remove(victimPrefix.itemKey);
+      Chunk newChunk = new Chunk(prefix);
+      if (currentSize < maximumSize) {
+        // insert if there's enough space in the cache
+        insertChunkToPrefix(prefix, newChunk);
+      } else {
+        // if exists, evict a chunk (victim) if there's no space
+        Chunk victim = findVictim(newChunk);
+        if (victim == null) {
+          // no suitable victim found and the cache is full - stop inserting
+          for (int i = 0; prefix.size() < idealSize && i < idealSize - prefix.size(); i++) {
+            // record rejection for each chunk that could not be inserted
+            policyStats.recordRejection();
+          }
+          break;
+        }
+        removeChunkFromPrefix(victim.fatherPrefix);
+        insertChunkToPrefix(prefix, newChunk);
       }
     }
+  }
+
+  private void removeChunkFromPrefix(Prefix victimPrefix) {
+    victimPrefix.removeChunk();
+    currentSize--;
+    if (victimPrefix.size() == 0) {
+      data.remove(victimPrefix.itemKey);
+    }
+    policyStats.recordOperation();
+    policyStats.recordEviction();
+  }
+
+  private void insertChunkToPrefix(Prefix prefix, Chunk newChunk) {
+    prefix.insertChunk(newChunk);
+    currentSize++;
+    policyStats.recordOperation();
+    policyStats.recordAdmission();
+  }
+
+  /**
+   * @param newChunk the new chunk to be inserted
+   * @return the victim chunk to be evicted, or null if no suitable one is found
+   */
+  @Nullable
+  private Chunk findVictim(Chunk newChunk) {
+    ArrayList<Chunk> victimCandidates = getAllEndChunks();
+    double sourceDelay = sampleSourceProcessingTime();
+    ArrayList<Chunk> suitableVictims = findSuitableVictims(newChunk, victimCandidates, sourceDelay);
+    return getLowestEvictionBenefitChunk(suitableVictims, sourceDelay);
   }
 
   @Nullable
-  private Chunk findVictim(Chunk newChunk, ArrayList<Chunk> victimCandidates) {
+  private Chunk getLowestEvictionBenefitChunk(ArrayList<Chunk> possibleVictims, double sourceDelay) {
+    if (possibleVictims.isEmpty()) {
+      return null;
+    }
+
+    policyStats.recordOperation();
+    Chunk victim = possibleVictims.getFirst();
+    for (Chunk candidate : possibleVictims) {
+      if (evictionBenefit(candidate, sourceDelay) < evictionBenefit(victim, sourceDelay)) {
+        victim = candidate;
+      }
+    }
+    return victim;
+  }
+
+  /**
+   * @param newChunk         the new chunk to be inserted
+   * @param victimCandidates the list of possible victims - all end chunks
+   * @param sourceDelay      the source processing time
+   * @return list of possible victims that can be evicted -
+   * those that have a lower eviction benefit than the new chunk insertion benefit
+   */
+  private ArrayList<Chunk> findSuitableVictims(
+    Chunk newChunk,
+    ArrayList<Chunk> victimCandidates,
+    double sourceDelay
+  ) {
+    policyStats.recordOperation();
     ArrayList<Chunk> possibleVictims = new ArrayList<>();
     for (Chunk candidate : victimCandidates) {
-      if (benefit(newChunk) >= benefit(candidate)) {
+      if (insertionBenefit(newChunk, sourceDelay) >= evictionBenefit(candidate, sourceDelay)) {
         possibleVictims.add(candidate);
       }
     }
-    return possibleVictims.stream()
-      .min(Comparator.comparingDouble(Chunk::frequency))
-      .orElse(null);
+    return possibleVictims;
   }
 
-  private double benefit(Chunk chunk) {
-    double possibilityForRequest = Math.exp(chunk.requestIndex - currentRequest);
-    return possibilityForRequest * chunk.frequency;
+  private double calculateDelay(double sourceDelay, long prefixSize, long bandwidth) {
+    if (prefixSize < 0) {
+      throw new InvalidParameterException("Prefix size must be non-negative!");
+    }
+    return sourceDelay - (double) prefixSize / bandwidth;
+  }
+
+  private double insertionBenefit(Chunk chunk, double sourceDelay) {
+    // calculate the benefit of inserting a new chunk to its prefix
+    // D_i[r] = T[s] - (|P_i[r]| + 1) / B
+    double newDelay = calculateDelay(sourceDelay, chunk.fatherPrefix.size() + 1, BANDWIDTH);
+    return 1 / Math.pow(newDelay, 2) * chunk.fatherPrefix.frequency;
+  }
+
+  private double evictionBenefit(Chunk chunk, double sourceDelay) {
+    // calculate the benefit of evicting the last chunk from its prefix
+    // D_i[r] = T[s] - (|P_i[r]| - 1) / B
+    double newDelay = calculateDelay(sourceDelay, chunk.fatherPrefix.size() - 1, BANDWIDTH);
+    return 1 / Math.pow(newDelay, 2) * chunk.fatherPrefix.frequency;
   }
 
   private ArrayList<Chunk> getAllEndChunks() {
+    policyStats.recordOperation();
     ArrayList<Chunk> endChunks = new ArrayList<>();
     for (Prefix prefix : data.values()) {
       endChunks.add(prefix.chunks.peek());
@@ -116,7 +201,7 @@ public final class NonBinaryPolicy implements Policy {
 
   @Override
   public PolicyStats stats() {
-    return null;
+    return policyStats;
   }
 
   @Override
@@ -124,22 +209,25 @@ public final class NonBinaryPolicy implements Policy {
     return Policy.super.name();
   }
 
-  record Chunk(long itemKey, long requestIndex, long frequency) {
-
-    public Chunk(long itemKey, long requestIndex) {
-      this(itemKey, requestIndex, 1);
-    }
+  record Chunk(Prefix fatherPrefix) {
   }
 
   static class Prefix {
-    final long itemKey, requestIndex, frequency;
+    final long itemKey, frequency;
     Stack<Chunk> chunks;
 
-    public Prefix(long itemKey, long requestIndex) {
+    public Prefix(long itemKey) {
       this.itemKey = itemKey;
-      this.requestIndex = requestIndex;
       this.frequency = 1;
       this.chunks = new Stack<>();
+    }
+
+    public void insertChunk(Chunk chunk) {
+      chunks.push(chunk);
+    }
+
+    public void removeChunk() {
+      chunks.pop();
     }
 
     public long size() {
