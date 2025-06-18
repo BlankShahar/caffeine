ef6bb1b5927ec0a7eb558f2d8ab0013b2ff110d2
ShaharBlank <blanksh@post.bgu.ac.il>
2025-06-18 19:45:06 +0300

    Print policy runtime when finished running

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/Simulator.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/Simulator.java
@@ -65,7 +65,9 @@ public final class Simulator {
     settings = new BasicSettings(config.getConfig("caffeine.simulator"));
   }
 
-  /** Broadcast the trace events to all of the policy actors. */
+  /**
+   * Broadcast the trace events to all of the policy actors.
+   */
   public void run() {
     var trace = getTraceReader(settings);
     var policies = getPolicyActors(trace.characteristics());
@@ -107,7 +109,7 @@ public final class Simulator {
 
       var futures = policies.stream()
         .map(policy -> policy.completed().thenRun(() ->
-          System.out.println("Finished running policy: " + policy.getPolicyName())
+          System.out.println("Finished running policy: " + policy.getPolicyName() + " (" + policy.stats().stopwatch() + ")")
         ))
         .toArray(CompletableFuture[]::new);
 
@@ -121,7 +123,9 @@ public final class Simulator {
     reporter.print(results);
   }
 
-  /** Returns a trace reader for the access events. */
+  /**
+   * Returns a trace reader for the access events.
+   */
   private static TraceReader getTraceReader(BasicSettings settings) {
     if (settings.trace().isSynthetic()) {
       return Synthetic.generate(settings.trace());
@@ -131,15 +135,19 @@ public final class Simulator {
     return format.readFiles(filePaths);
   }
 
-  /** Returns the policy actors that asynchronously apply the trace events. */
+  /**
+   * Returns the policy actors that asynchronously apply the trace events.
+   */
   private ImmutableList<PolicyActor> getPolicyActors(Set<Characteristic> characteristics) {
     var registry = new Registry(settings, characteristics);
     return registry.policies().stream()
-        .map(policy -> new PolicyActor(Thread.currentThread(), policy, settings))
-        .collect(toImmutableList());
+      .map(policy -> new PolicyActor(Thread.currentThread(), policy, settings))
+      .collect(toImmutableList());
   }
 
-  /** Throws the underlying cause for the simulation failure. */
+  /**
+   * Throws the underlying cause for the simulation failure.
+   */
   private static void throwError(RuntimeException error, Iterable<PolicyActor> policies) {
     if (!Thread.currentThread().isInterrupted()) {
       throw error;
