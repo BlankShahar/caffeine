17def299c9ccf4447a328d04029bf00e25e13f93
ShaharBlank <blanksh@post.bgu.ac.il>
2025-07-07 20:50:16 +0300

    Fix parameters and consts for the Network (CDN) use-case

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/non_binary/Consts.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/non_binary/Consts.java
@@ -1,10 +1,10 @@
 package com.github.benmanes.caffeine.cache.simulator.policy.non_binary;
 
 public final class Consts {
-  public static final double CHUNK_SIZE = 1; // in MB
-  public static final long BANDWIDTH = 1000; // in MBps
+  public static final double CHUNK_SIZE = 20; // in MB
+  public static final long BANDWIDTH = 100; // in MBps
   public static final long REQUESTS_FREQUENCY_PERIOD = 10_000;
   public static final int SOURCE_KEY = 1;
-  public static final double SOURCE_MEAN = 0.003;
-  public static final double SOURCE_STD = 0.00075;
+  public static final double SOURCE_MEAN = 0.04;
+  public static final double SOURCE_STD = 0.0004;
 }
