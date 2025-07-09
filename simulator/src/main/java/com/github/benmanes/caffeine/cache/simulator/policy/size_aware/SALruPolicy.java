1da42792f76cc127a89e1fa85dd87429ff637796
ShaharBlank <blanksh@post.bgu.ac.il>
2025-07-10 00:13:39 +0300

    Remove print

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/size_aware/SALruPolicy.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/size_aware/SALruPolicy.java
@@ -38,7 +38,7 @@ public final class SALruPolicy implements Policy {
 
     policyStats.recordOperation();
     currentTime++;
-    System.out.println(currentTime);
+//    System.out.println(currentTime);
 
     Item item = data.get(itemKey);
     if (item != null) {
