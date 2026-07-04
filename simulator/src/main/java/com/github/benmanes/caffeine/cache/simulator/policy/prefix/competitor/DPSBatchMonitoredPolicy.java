e4e0b7a14595ccb5b965b9ebf5b21520be587bba
Shahar Blank <shaharblank1@gmail.com>
2026-07-04 23:31:47 +0300

    Fix policy spec names

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/prefix/competitor/DPSBatchMonitoredPolicy.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/prefix/competitor/DPSBatchMonitoredPolicy.java
@@ -3,9 +3,9 @@ package com.github.benmanes.caffeine.cache.simulator.policy.prefix.competitor;
 import com.github.benmanes.caffeine.cache.simulator.policy.Policy;
 import com.typesafe.config.Config;
 
-@Policy.PolicySpec(name = "prefix.competitor.DPSBatch.Monitored")
+@Policy.PolicySpec(name = "prefix.competitor.SBatch.DP.Monitored")
 public final class DPSBatchMonitoredPolicy extends AbstractDPSBatchPolicy {
   public DPSBatchMonitoredPolicy(Config config) {
-    super(config, Mode.MONITORED, "prefix.competitor.DPSBatch.Monitored");
+    super(config, Mode.MONITORED, "prefix.competitor.SBatch.DP.Monitored");
   }
 }
--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/prefix/competitor/DPSBatchOraclePolicy.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/prefix/competitor/DPSBatchOraclePolicy.java
@@ -3,9 +3,9 @@ package com.github.benmanes.caffeine.cache.simulator.policy.prefix.competitor;
 import com.github.benmanes.caffeine.cache.simulator.policy.Policy;
 import com.typesafe.config.Config;
 
-@Policy.PolicySpec(name = "prefix.competitor.DPSBatch.Oracle")
+@Policy.PolicySpec(name = "prefix.competitor.SBatch.DP.Oracle")
 public final class DPSBatchOraclePolicy extends AbstractDPSBatchPolicy {
   public DPSBatchOraclePolicy(Config config) {
-    super(config, Mode.ORACLE, "prefix.competitor.DPSBatch.Oracle");
+    super(config, Mode.ORACLE, "prefix.competitor.SBatch.DP.Oracle");
   }
 }
